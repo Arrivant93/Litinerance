ce8de195064be05197b058a86dc3e486b4aaa13a
KiSEiGitHub <tommusique974@gmail.com>
2023-06-28 13:08:16 +0200

    :feat: setup tailwind

--- src/pages/home.php
+++ src/pages/home.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="../../public/css/style.css">
 </head>
 
 <body>
-    <h1>Home</h1>
+    <h1 class="text-center">Home</h1>
 </body>
 
 </html>
